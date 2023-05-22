71f8eb2c03a499100a339d351cf3533643084b7c
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-05-22 16:04:17 +0200

    Add CRUD on user and ad tests.

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -12,7 +12,43 @@ describe('app', () => {
   });
 
   it('should return 200 for the root route', async () => {
-    const response = await request(app).get('/api/user/c3c3a799-61b4-4b30-a10e-237056bd5c85');
+    const response = await request(app).get('/api/user');
+    expect(response.status).toEqual(200);
+  });
+});
+
+describe('user', () => {
+  it('should create a user', async () => {
+    await request(app).post('/api/user').send({
+      email: 'test@test.com',
+      name: 'test',
+      accounts: [
+        {
+          id: "1cee38a1-a84b-4b7a-8ac7-5fdfd169dedd",
+        },
+      ],
+    });
+  });
+  it('should get all users', async () => {
+    const response = await request(app).get('/api/user');
+    expect(response.status).toEqual(200);
+  });
+  it('should get a user by id', async () => {
+    const userId = await request(app).get('/api/user').then(res => res.body.body.users[0].id);
+    const response = await request(app).get(`/api/user/${userId}`);
+    expect(response.status).toEqual(200);
+  });
+  it('should update a user by id', async () => {
+    const userId = await request(app).get('/api/user').then(res => res.body.body.users[0].id);
+    const response = await request(app).put(`/api/user/${userId}`).send({
+      email: 'testing@test.com',
+      name: 'testing'
+    });
+    expect(response.status).toEqual(200);
+  });
+  it('should delete a user by id', async () => {
+    const userId = await request(app).get('/api/user').then(res => res.body.body.users[0].id);
+    const response = await request(app).delete(`/api/user/${userId}`);
     expect(response.status).toEqual(200);
   });
 });
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -5,6 +5,18 @@ const express = require('express');
 const router = express.Router();
 const prisma = new PrismaClient();
 
+router.get("/", async (req, res) => {
+	try {
+
+		const users = await prisma.user.findMany({ include: { accounts: true } });
+
+		return res.send({ status: 200, body: { users } });
+
+	} catch (error) {
+		return res.status(502).json({ error: "Something went wrong" });
+	}
+});
+
 router.get("/:id", async (req, res) => {
 	try {
 
@@ -14,7 +26,7 @@ router.get("/:id", async (req, res) => {
 
 
 		if (!user) {
-			return res.send({ status: 404, body: { message: 'Données incorrect' } });
+			return res.send({ status: 404, body: { message: 'Données incorrectes' } });
 		}
 
 		return res.send({ status: 200, body: { user } });
@@ -38,6 +50,64 @@ router.post("/:id/accounts/credit", async (req, res) => {
 
 	} catch (error) {
 		return res.status(502).json({ error: "Something went wrong" });
+	}
+});
+
+router.put("/:id", async (req, res) => {
+	try {
+
+		const { id } = req.params
+
+		const user = await prisma.user.findUnique({ where: { id } });
+
+		if (!user) {
+			return res.send({ status: 404, body: { message: 'Données incorrectes' } });
+		}
+
+		const updatedUser = await prisma.user.update({
+			where: { id },
+			data: {
+				...req.body
+			}
+		});
+
+		return res.send({ status: 200, body: { updatedUser } });
+
+	} catch (error) {
+		return res.status(502).json({ error: error.message });
+	}
+});
+
+router.delete("/:id", async (req, res) => {
+	try {
+
+		const { id } = req.params
+
+		const user = await prisma.user.findUnique({ where: { id } });
+
+		const accounts = await prisma.account.findMany({ where: { userId: id } });
+
+		if (!user) {
+			return res.send({ status: 404, body: { message: 'Données incorrectes' } });
+		}
+
+		if (accounts.length > 0) {
+			accounts.forEach(async (account) => {
+				await prisma.account.update({
+					where: { id: account.id },
+					userId: null
+				});
+			});
+		}
+
+		const deletedUser = await prisma.user.delete({
+			where: { id }
+		});
+
+		return res.send({ status: 200, body: { deletedUser } });
+
+	} catch (error) {
+		return res.status(502).json({ error: error.message });
 	}
 });
 
